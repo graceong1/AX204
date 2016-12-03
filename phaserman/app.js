98de775d62659ee944460476f641beb5eeba6705
Grace Ong <graceong@MakeshiftStockImage-86.local>
2016-12-03 12:28:27 +0800

    fixing player

--- phaserman/app.js
+++ phaserman/app.js
@@ -1,101 +1,112 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload:preload, create: create, update: update});
+console.log('hey hey it works');
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var score = 0;
 var life = 3;
 
 function preload() {
-	game.load.image('sky', 'assets/sky.png');
-	game.load.image('ground', 'assets/platform.png');
-	game.load.image('star', 'assets/star.png');
-	//loading spritesheet is different, you need to write width + height too
-	game.load.spritesheet('dude', 'assets/dude.png', 32, 48); 
-	game.loag.spritesheet('baddie', 'assets/baddie.png', 32, 32);
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
 }
 
 function create() {
-	//setting up whole physics engine
-	game.physics.startSystem(Phaser.Physics.ARCADE); 
-	// sky
-	game.add.sprite(0, 0, 'sky');
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.add.sprite(0, 0, 'sky');
 
-	// group of platforms
-	platforms = game.add.physicsGroup(); //creating new rule, creating new group
-	platforms.enableBody = true; //able to grap platforms as whole and change it
+  // Making group of platforms
+  platforms = game.add.physicsGroup();
+  platforms.enableBody = true;
 
-	// ground
-	var ground = platforms.create(0, game.world.height - 50, 'ground'); //game.world.height - 50 = 550
-	ground.scale.setTo (2, 2);
-	ground.body.immovable = true;
+  // Ground
+  var ground = platforms.create(0, game.world.height - 50, 'ground');
+  ground.scale.setTo(2, 2);
+  ground.body.immovable = true;
 
-	// ledges
-	var ledge = platforms.create(400, 400, 'ground');
-	ledge.body.immovable = true;
-	ledge = platforms.create(-150, 250, 'ground');
-	ledge.body.immovable = true;
+  // Ledges
+  var ledge = platforms.create(400, 400, 'ground');
+  ledge.body.immovable = true;
+  ledge = platforms.create(-150, 250, 'ground');
+  ledge.body.immovable = true;
 
-	  // Player
-	  player = game.add.sprite(32, game.world.height - 220, 'dude');
-	    // player animations using spritesheet and applies game physics
-	    player.animations.add('left', [0, 1, 2, 3], 10, true);
-	    player.animations.add('right', [5, 6, 7, 8], 10, true);
-	    game.physics.arcade.enable(player);
-	    // Player physics properties. Give the little guy a slight bounce.
-	    player.body.bounce.y = 0.2;
-	    player.body.gravity.y = 300;
-	    player.body.collideWorldBounds = true;
+  // Player
+  player = game.add.sprite(32, game.world.height - 220, 'dude');
+    // player animations using spritesheet and applies game physics
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    game.physics.arcade.enable(player);
+    // Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 
-	// enemies
-	enemy1 = game.add.sprite(760, 520, 'baddie');
-		// animate sprite 
-		enemy1.animations.add('left', [0, 1] 10, true); 
-		enemy1.animations.add('right', [3, 4] 10, true);
-		// add physics
-		game.physics.arcade.enable(enemy1);
-		enemy1.body.bounce.y = 0.2; 
-		enemy1.body.gravity.y = 500; 
-		enemy1.body.collideWorldBounds = true;
+  // Enemies
+  enemy1 = game.add.sprite(760, 20, 'baddie')
+    // Enemy animations using spritesheet and applies game physics
+    enemy1.animations.add('left', [0, 1], 10, true);
+    enemy1.animations.add('right', [2, 3], 10, true);
+    game.physics.arcade.enable(enemy1);
+    // Enemy physics properties.
+    enemy1.body.bounce.y = 0.2;
+    enemy1.body.gravity.y = 500;
+    enemy1.body.collideWorldBounds = true;
 
-	enemy2 = game.add.sprite(10, 20, 'baddie');
-		// animate sprite 
-		enemy2.animations.add('left', [0, 1] 10, true); 
-		enemy2.animations.add('right', [3, 4] 10, true);
-		// add physics
-		game.physics.arcade.enable(enemy2);
-		enemy2.body.bounce.y = 0.2; 
-		enemy2.body.gravity.y = 500; 
-		enemy2.body.collideWorldBounds = true;
+  enemy2 = game.add.sprite(10, 20, 'baddie')
+    // Enemy animations using spritesheet and applies game physics
+    enemy2.animations.add('left', [0, 1], 10, true);
+    enemy2.animations.add('right', [2, 3], 10, true);
+    game.physics.arcade.enable(enemy2);
+    // Enemy physics properties.
+    enemy2.body.bounce.y = 0.2;
+    enemy2.body.gravity.y = 500;
+    enemy2.body.collideWorldBounds = true;
 
-	enemy3 = game.add.sprite(200, 20, 'baddie');
-		// animate sprite 
-		enemy3.animations.add('left', [0, 1] 10, true); 
-		enemy3.animations.add('right', [3, 4] 10, true);
-		// add physics
-		game.physics.arcade.enable(enemy3);
-		enemy3.body.bounce.y = 0.2; 
-		enemy3.body.gravity.y = 500; 
-		enemy3.body.collideWorldBounds = true;
+  enemy3 = game.add.sprite(200, 20, 'baddie')
+    // Enemy animations using spritesheet and applies game physics
+    enemy3.animations.add('left', [0, 1], 10, true);
+    enemy3.animations.add('right', [2, 3], 10, true);
+    game.physics.arcade.enable(enemy3);
+    // Enemy physics properties.
+    enemy3.body.bounce.y = 0.2;
+    enemy3.body.gravity.y = 500;
+    enemy3.body.collideWorldBounds = true;
 
-	// set up keyboard events
-	cursors = game.input.keyboard.createCursorKeys();
+  // Creating keyboard entry
+  cursors = game.input.keyboard.createCursorKeys();
 
-	// create stars
-	stars = game.add.physicsGroup();
-	stars.enableBody = true;
-	// loop to create 12 stars
-	for(var i = 0; 1 < 12; i++) {
-		var star = stars.create(1 * 70, 0, 'star');
-		star.body.gravity.y = 200; // gravity.y equals gravity up and down
-		star.body.bounce.y = 0.2 + Math.random * 0.7;
-	}
+  // Creating stars
+  stars = game.add.physicsGroup();
+  stars.enableBody = true;
+  //  Here we'll create 12 of them evenly spaced apart
+  for (var i = 0; i < 12; i++){
+    //  Create a star inside of the 'stars' group
+    var star = stars.create(i * 70, 0, 'star');
+    //  Let gravity do its thing
+    star.body.gravity.y = 200;
+    //  This just gives each star a slightly random bounce value
+    star.body.bounce.y = 0.7 + Math.random() * 0.2;
+  }
 
-	// set up text
-	var style = {font: "bold 32px Arial", fill: "#fff", boundsAlineH: "center", boundsAlginV: "middle"};
-	// create and position text
-	scorelabel = game.add.text(-60, 0, "Your score is: ", style);
-	scoretext = game.add.text(70, 0, score, style);
-	scorelabel.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
-	scoretext.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
-	scorelabel.setTextBounds(0, 520, 80, 100); // 0 & 520 are x & y, 80 & 100 are width & height
-	scoretext.setTextBounds(0, 520, 80, 100);
+  // Setting style for text
+  var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+    //  The Text is positioned at these coordinates within textbound
+    scorelabel = game.add.text(-60, 0, "Your score is: ", style);
+    scoretext = game.add.text(70, 0, score, style);
+    scorelabel.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    scoretext.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    //  We'll set the bounds to be from x0, y520 (top down) and be 800px wide by 100px high
+    scorelabel.setTextBounds(0, 520, 800, 100);
+    scoretext.setTextBounds(0, 520, 800, 100);
+    //  Doing the same for lives count
+    lifelabel = game.add.text(-300, 0, "Lives: ", style);
+    lifetext = game.add.text(-240, 0, life, style);
+    lifelabel.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    lifetext.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+    //  We'll set the bounds to be from x0, y520 (top down) and be 800px wide by 100px high
+    lifelabel.setTextBounds(0, 0, 800, 100);
+    lifetext.setTextBounds(0, 0, 800, 100);
 }
 
 function update() {
